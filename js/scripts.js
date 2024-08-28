0341410e1a1dda2083bb4cdb549342330b71e92b
Michael Nguyen <nguyenmichaelkhang@gmail.com>
2024-08-28 01:50:32 -0400

    added email ,phone, password toggle

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,36 @@ loginLink.addEventListener('click', ()=> {
     document.getElementById('regWindow').style.display = 'none';
     document.getElementById('loginWindow').style.display = 'block';
 });
+
+let eyeIconLogin = document.querySelector("#showPasswordLogin");
+let eyeIconReg = document.querySelector("#showPasswordReg");
+let logPassword = document.querySelector("#loginPassword");
+let regPassword = document.querySelector("#regPassword");
+
+eyeIconLogin.addEventListener('click', ()=> {
+    if (logPassword.type == 'password')
+    {
+        logPassword.type = 'text';
+        eyeIconLogin.src = '/images/eye-open.png';
+    }
+    else
+    {
+        logPassword.type = 'password';
+        eyeIconLogin.src = '/images/eye-close.png';
+    }
+
+});
+
+eyeIconReg.addEventListener('click', ()=> {
+    if (regPassword.type == 'password')
+    {
+        regPassword.type = 'text';
+        eyeIconReg.src = '/images/eye-open.png';
+    }
+    else
+    {
+        regPassword.type = 'password';
+        eyeIconReg.src = '/images/eye-close.png';
+    }
+
+});
