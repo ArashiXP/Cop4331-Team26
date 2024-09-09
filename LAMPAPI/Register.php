5ab157520af2cc609447faf08d33cfcc08a66ac6
GoldfishBrett <108097466+GoldfishBrett@users.noreply.github.com>
2024-09-09 01:14:29 -0400

    Check if username already exists in Register API

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,35 +1,43 @@
 <?php
 
-  $inData = getRequestInfo();
+    $inData = getRequestInfo();
 
-  $firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-  $login = $inData["login"];
-  $password = $inData["password"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $login = $inData["login"];
+    $password = $inData["password"];
+    $isValid = true;
 
-  $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
 	}
-  else
-  { 
-    $stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-    $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-    $stmt->execute();
+    else
+    {
+        global $isValid;
+        validateInformation($conn, $login, $isValid);
 
-    $result = $stmt->get_result();
-    returnWithInfo($firstName, $lastName, $login, $password);
+        if($isValid)
+        {
+            $stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+            $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            returnWithInfo($firstName, $lastName, $login, $password);
 
 
-    $stmt->close();
-    $conn->close();
+            $stmt->close();
+        }
+
+        $conn->close();
 
     
-  }
+    }
 
 
-  function getRequestInfo()
+    function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
@@ -46,11 +54,25 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
-  function returnWithInfo( $firstName, $lastName, $login, $password)
-	{
+    function returnWithInfo( $firstName, $lastName, $login, $password)
+    {
 		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","login":"' . $login . '","password":"' . $password . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
-	
+    function validateInformation( $conn, $login, $isValid )
+    {
+        global $isValid;
+        //validation for username
+        $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if( $result->fetch_assoc() )
+        {
+            returnWithError("Username already taken!");
+            $isValid = false;
+        }
+        $stmt->close();
+    }
+
 ?>
