a53ab47a4bb508f80030488b57d60033649abe9d
GoldfishBrett <108097466+GoldfishBrett@users.noreply.github.com>
2024-09-09 00:38:19 -0400

    Add validation to AddContacts API

--- LAMPAPI/AddContacts.php
+++ LAMPAPI/AddContacts.php
@@ -7,6 +7,7 @@
 	$name = $inData["name"];
 	$phone = $inData["phone"];
 	$email = $inData["email"];
+	$isValid = true;
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
@@ -15,13 +16,19 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (userid, name, phone, email) VALUES (?, ?, ?, ?)");
-		$stmt->bind_param("isss", $userid, $name, $phone, $email);
-		$stmt->execute();
+		global $isValid;
+		validateInformation($conn, $userid, $name, $phone, $email, $isValid);
 
-		returnWithInfo($userid, $name, $phone, $email);
+		if ($isValid)
+		{
+			$stmt = $conn->prepare("INSERT into Contacts (userid, name, phone, email) VALUES (?, ?, ?, ?)");
+			$stmt->bind_param("isss", $userid, $name, $phone, $email);
+			$stmt->execute();
 
-		$stmt->close();
+			returnWithInfo($userid, $name, $phone, $email);
+
+			$stmt->close();
+		}
 		$conn->close();
 	}
 	
@@ -47,6 +54,34 @@
 		$retValue = '{"userid":' . $userid . ',"name":"' . $name . '","phone":"' . $phone .
 			'","email":"'  . $email . '","error":""}';
 		sendResultInfoAsJson( $retValue );
+	}
+
+	function validateInformation( $conn, $userid, $name, $phone, $email, $isValid )
+	{
+		global $isValid;
+		//validation for user ID
+		$stmt = $conn->prepare("SELECT Login FROM Users WHERE ID=?");
+		$stmt->bind_param("i", $userid);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if( !($result->fetch_assoc())  )
+		{
+			returnWithError("Invalid UserID");
+			$isValid = false;
+		}
+		//validation for phone number
+		if(!preg_match('/^\d{3}-\d{3}-\d{4}$/', $phone))
+		{
+			returnWithError("Invalid Phone Number");
+			$isValid = false;
+		}
+		//validation for email address
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			returnWithError("Invalid Email Address");
+			$isValid = false;
+		}
+		$stmt->close();
 	}
 	
 ?>
