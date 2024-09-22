c6a668a728abe47352643dee8329cec98fb5f8dc
Michael Nguyen <nguyenmichaelkhang@gmail.com>
2024-09-22 15:45:49 -0400

    fixed add and added search

--- js/scripts.js
+++ js/scripts.js
@@ -238,13 +238,8 @@ searchButton.addEventListener('click', ()=>{
 
 nameSearch.addEventListener('keydown', ()=> {
 	if (event.key === 'Enter')
-		printText();
+		searchContacts();
 });
-
-function printText()
-{
-	console.log(nameSearch.value);
-}
 
 addContactButton.addEventListener('click', ()=> {
 	document.getElementById('addName').style.display = 'block';
@@ -259,7 +254,20 @@ function addContacts()
 	let phone = document.getElementById('contactsPhoneNum').value;
 	let name = first + ' ' + last
 
+	if(!phone.includes("-"))
+	{
+		phone = phone.slice(0,3) + "-" + phone.slice(3,6) + "-" + phone.slice(6,15);
+	}
+
+	if(!validContact(first,last,email,phone))
+		{
+			console.log("Something is wrong with the contact");
+			return;
+		}
+
 	let contact = {name:name, email:email, phone:phone, userid:userId};
+
+
 
 	let jsonPayload = JSON.stringify(contact);
 
@@ -275,6 +283,7 @@ function addContacts()
                 showContacts();
 				document.getElementById('addName').style.display = 'none';
 				document.getElementById('contactsWindow').style.display = 'block';
+				clearText();
 				console.log("added: " + name, email, phone,userId);
             }
         };
@@ -283,6 +292,14 @@ function addContacts()
         console.log(err.message);
 	}
 
+}
+
+function clearText() 
+{
+	document.getElementById('contactsFName').value = '';
+	document.getElementById('contactsLName').value = '';
+	document.getElementById('contactsEmail').value = '';
+	document.getElementById('contactsPhoneNum').value = '';
 }
 
 function showContacts()
@@ -307,23 +324,24 @@ function showContacts()
 						console.log(jsonObject.error);
 						return;
                 	} 
-					let collection = "<table class=\"contactsTable\">";
-					for (let i = 0; i < Object.keys(jsonObject.results); i++)
+					let collection = "";
+					for (let i = 0; i < Object.keys(jsonObject).length; i++)
 					{
-						let splitName = jsonObject.results[i].name.split(" ");
+						let splitName = jsonObject[i].name.split(" ");
 						let fName = splitName[0];
 						let lName = splitName[1];
-						console.log(fName + lName);
-						allIds[i] = jsonObject.results.length[i].ID
+						// console.log("This is split: " + fName + " " + lName);
+						// allIds[i] = ObjejsonObject.length[i].ID
 						collection += "<tr>"
-						collection += "<td id = \"listFirst\"\>" + fName + "</td>"
-						collection += "<td id = \"listLast\"\>" + lName + "</td>"
-						collection += "<td id = \"listEmail\"\>" + jsonObject.results[i].email + "</td>"
-						collection += "<td id = \"listPhone\"\>" + jsonObject.results[i].phone + "</td>"
+						collection += "<td id = \"listFirst" + i +  "\"><span>" + fName + "</span></td>"
+						collection += "<td id = \"listLast" + i +  "\"><span>" + lName + "</span></td>"
+						collection += "<td id = \"listEmail" + i +  "\"><span>" + jsonObject[i].email + "</span></td>"
+						collection += "<td id = \"listPhone" + i +  "\"><span>" + jsonObject[i].phone + "</span></td>"
 						collection += "</tr>"
 					}
 					collection += "</table>"
-					document.getElementById(tbody).innerHTML = collection;
+					// console.log("This is collection: \n" + collection);
+					document.getElementById("listContacts").innerHTML = collection;
 				}
 					};
 					xhr.send(jsonPayload);
@@ -333,13 +351,113 @@ function showContacts()
 						}
 }
 
-// function validContact(first, last, email, phone)
-// {
-// 	let firstE, lastE, emailE, phoneE = true;
+function searchContacts()
+{
+	let first, last;
+	let table = document.getElementById("contactsTable");
+	let row = table.getElementsByTagName("tr");
+	let input = document.getElementById("searchInput");
+	let fixedInput = input.value.toLowerCase().split(" ");
 
-// 	if(first == "")
+	for (let i = 0; i < row.length; i++)
+	{
+		first = row[i].getElementsByTagName("td")[0];
+		last = row[i].getElementsByTagName("td")[1];
 
-// }
+		if (first && last)
+		{
+			let firstTableText = first.textContent || first.innerText;
+			let lastTableText = last.textContent || last.innerText;
+			row[i].style.display = "none";
+
+			for (let j = 0; j < fixedInput.length; j++) {
+                let element = fixedInput[j];
+                if (firstTableText.toLowerCase().indexOf(element) >= 0) 
+				{
+                    row[i].style.display = "";
+                }
+                if (lastTableText.toLowerCase().indexOf(element) >= 0) 
+				{
+                    row[i].style.display = "";
+                }
+			}
+		}
+	}
+}
+
+function validContact(first, last, email, phone)
+{
+	let firstE, lastE, emailE, emailV, phoneE, phoneV = false;
+	let text = "Please Fill In Your:<br>"
+
+	if(first == "")
+	{
+		text+= "First Name<br>";
+		firstE = true;
+	}
+
+	if(last == "")
+	{
+		text += "Last Name<br>";
+		lastE = true;
+	}
+
+	if(email == "")
+	{
+		text+= "Email<br>";
+		emailE = true;
+	}
+	else
+	{
+		let vEmail = new RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+		if (vEmail.test(email) == false)
+		{
+			emailV = true;
+		}
+	}
+
+	if(phone == "")
+	{
+		text += "Phone\n";
+		phoneE = true;
+	}
+	else
+	{
+		let vPhone = new RegExp(/^[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/);
+		if (vPhone.test(phone) == false)
+		{
+			phoneV = true;
+		}
+	}
+
+	if(!(firstE || lastE || emailE || phoneE))
+	{
+		text = "";
+	}
+
+	if((phoneV || emailV))
+	{
+		text += "Invalid:<br>";
+		if(phoneV)
+		{
+			text += "Phone Number<br>";
+		}
+		if (emailV)
+		{
+			text += "Email";
+		}
+	}
+
+	if((firstE || lastE || emailE || emailV || phoneE || phoneV))
+	{
+		console.log("should have returned false");
+		document.getElementById("addResult").innerHTML = text;
+		return false;
+	}
+
+	console.log("returned true");
+	return true;
+}
 
 } // End of Contacts Page
 
